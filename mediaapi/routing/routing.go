db6a0ce8f4b3e5c0514eb467c61a60b28ddf26b2
Andrew Morgan <andrew@amorgan.xyz>
2019-06-17 15:34:05 +0100

    Replace deprecated method

--- mediaapi/routing/routing.go
+++ mediaapi/routing/routing.go
@@ -28,7 +28,6 @@ import (
 	"github.com/matrix-org/dendrite/mediaapi/types"
 	"github.com/matrix-org/gomatrixserverlib"
 	"github.com/matrix-org/util"
-	"github.com/prometheus/client_golang/prometheus"
 )
 
 const pathPrefixR0 = "/_matrix/media/r0"
@@ -79,7 +78,7 @@ func makeDownloadAPI(
 	activeRemoteRequests *types.ActiveRemoteRequests,
 	activeThumbnailGeneration *types.ActiveThumbnailGeneration,
 ) http.HandlerFunc {
-	return prometheus.InstrumentHandler(name, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	return promhttp.Handler(name, http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		req = util.RequestWithLogging(req)
 
 		// Set common headers returned regardless of the outcome of the request
